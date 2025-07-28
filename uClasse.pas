8108242642ecb3840489e28f6ba4bbc52c8e0ffe
erthal-guu <vplgugs@gmail.com>
2025-07-28 17:00:14 -0300

    Inicio do Projeto Sobre POO

--- uClasse.pas
+++ uClasse.pas
@@ -2,6 +2,38 @@ unit uClasse;
 
 interface
 
+   //Abstração
+   //Encapsulamento
+  type TPersonagem = class
+    Nome: String;
+    Vida:Integer;
+    Genero:String;
+    Nivel:Integer;
+    Dano:Integer;
+    Defesa:Integer;
+
+    constructor Create(aDano,aDefesa:Integer);
+
+  end;
+  //Herança
+  type TPlayer = class (TPersonagem)
+    NickName:String;
+    Experiência:Double;
+  end;
+
+  type TBoss = class (TPersonagem)
+
+  end;
+
+
 implementation
+
+{  Clsse : TPersonagem }
+
+constructor TPersonagem.Create(aDano, aDefesa: Integer);
+begin
+  Dano := aDano;
+  Defesa := aDefesa;
+end;
 
 end.
